7d538054dfab5832ca23793b7f09baf59d25f3f6
Neechelle James <neechelle.james@gmail.com>
2023-04-28 13:53:28 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record){
+    const win = record.find(obj => obj.result === 'W')
+    
+        if(win === undefined){
+            return undefined
+        }
+
+        if(win.result === 'W'){
+            return (win.year)
+    }
+       }
